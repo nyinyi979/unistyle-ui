56d6ae829b3f0e5b9f8a797404a47358b30036f8
nyinyi979 <nyinyi095062687@gmail.com>
2024-02-18 18:40:14 +0630

    fixes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var path_ = path.resolve('./lib');
 const fs = require('fs');
 const clc = require('cli-color');
 const simple_git = require('simple-git').simpleGit({
-    baseDir: './git',
+    baseDir: path.join(__dirname,'/git'),
     maxConcurrentProcesses: 6,
     trimmed: true,
 });
@@ -29,24 +29,24 @@ If the installation of each component is not working, you have probably deleted 
     handler: (argv) => {
 
         path_ = path.resolve(argv.path_name);
-        fs.mkdirSync(path_,{recursive:true});
-        fs.mkdirSync(`${path_}/ui`);
-        simple_git.clone('https://github.com/nyinyi979/unistyles_components.git',path.resolve('./git'),{},(e)=>{  
-            fs.cp('./git/lib/default_values',path.join(path_,'default_values'),{recursive:true},(err)=>{
+        !fs.existsSync(path_)&&fs.mkdirSync(path_,{recursive:true})&&fs.mkdirSync(`${path_}/ui`); 
+        simple_git.clone('https://github.com/nyinyi979/unistyles_components.git','/git',{},(e)=>{ 
+            console.log(e);
+            fs.cp(path.join(__dirname,'/git/lib/default_values'),path.join(path_,'default_values'),{recursive:true},(err)=>{
                 if(err) {
                     console.log(err);
                     return;
                 }
                 console.log('Installed default values');
-                fs.cp('./git/lib/utils',path.join(path_,'utils'),{recursive:true},(err)=>{
+                fs.cp(path.join(__dirname,'/git/lib/utils'),path.join(path_,'utils'),{recursive:true},(err)=>{
                     if(err){
                         console.log(err);
                         return;
                     }
                     console.log('Installed utils');
-                    fs.copyFileSync('./git/lib/default.d.ts',path.join(path_,'default.d.ts'));
-                    fs.copyFileSync('./git/lib/index.d.ts',path.join(path_,'index.d.ts'));
-                    fs.copyFile('./git/lib/unistyles.ts',path.join(path_,'unistyles.ts'),(err)=>{
+                    fs.copyFileSync(path.join(__dirname,'/git/lib/default.d.ts'),path.join(path_,'default.d.ts'));
+                    fs.copyFileSync(path.join(__dirname,'/git/lib/index.d.ts'),path.join(path_,'index.d.ts'));
+                    fs.copyFile(path.join(__dirname,'/git/lib/unistyles.ts'),path.join(path_,'unistyles.ts'),(err)=>{
                         if(err){
                             console.log(err);
                             return;
@@ -77,7 +77,7 @@ If the installation of each component is not working, you have probably deleted 
                     }
                     if(f.length === 0){
                         console.log("Found zero component installed, Installing all...");
-                        fs.cp('./git/lib/ui',path.join(path_,'ui'),{recursive:true},(e)=>{
+                        fs.cp(path.join(__dirname,'/git/ui'),path.join(path_,'ui'),{recursive:true},(e)=>{
                             if(e){
                                 console.log(e);
                                 return;
@@ -89,7 +89,7 @@ If the installation of each component is not working, you have probably deleted 
                         console.log(`Found ${f.map((v)=>`${clc.blue(v)},
 `)}Installing rest of the components`)
                         ui_components.forEach((c)=>{
-                            fs.copyFile(path.join(`./git/lib/ui/${c}.tsx`),path.join(`./${path_}/ui/${c}.tsx`),fs.constants.COPYFILE_EXCL,(e)=>{
+                            fs.copyFile(path.join(__dirname,`/git/lib/ui/${c}.tsx`),path.join(`./${path_}/ui/${c}.tsx`),fs.constants.COPYFILE_EXCL,(e)=>{
                                 if(e){
                                     if(e.code==='EEXIST') return;
                                     console.log(e);
@@ -105,12 +105,12 @@ If the installation of each component is not working, you have probably deleted 
 
             if(ui_components.has(argv.comp_name.toLowerCase())){
                 //node index install all
-                fs.copyFile(path.join(`./git/lib/ui/${argv.comp_name}.tsx`),path.join(`./${path_}/ui/${argv.comp_name}.tsx`),(err)=>{
+                fs.copyFile(path.join(__dirname,`/git/lib/ui/${argv.comp_name}.tsx`),path.join(`./${path_}/ui/${argv.comp_name}.tsx`),(err)=>{
                     if(err){
                         console.log(err);
                         return;
                     }
-                    console.log(clc.green(`Installed ${clc.bold(argv.cop_name)} to ${clc.blue(path_)}`))
+                    console.log(clc.green(`Installed ${clc.bold(argv.comp_name)} to ${clc.blue(path_)}`))
                 });
             }
             else console.log(clc.red("Not a valid component!"));
